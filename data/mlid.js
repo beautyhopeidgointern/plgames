54bbc3ecc215be1038f091a0096b630a50f26794
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-13 18:51:54 +0700

    Update and rename mobile-legends.js to mlid.js

--- data/mlid.js
+++ data/mlid.js
@@ -7,8 +7,6 @@ window.PRICE_DATA = {
     { name: "86 Diamond", price: "Rp 20.000" },
     { name: "172 Diamond", price: "Rp 39.000" },
     { name: "257 Diamond", price: "Rp 58.000" },
-    { name: "344 Diamond", price: "Rp 76.000" },
-    { name: "429 Diamond", price: "Rp 95.000" },
-    { name: "514 Diamond", price: "Rp 113.000" }
+    { name: "344 Diamond", price: "Rp 76.000" }
   ]
 };
