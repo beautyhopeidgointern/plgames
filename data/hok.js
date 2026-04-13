cc4cbf833ee26a3bbb38bbeda7c29a01d131d949
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-13 18:44:59 +0700

    Create hok.js

---
+++ data/hok.js
@@ -0,0 +1,10 @@
+window.PRICE_DATA = {
+  title: "Honor of Kings",
+  subtitle: "Top Up Token Murah",
+  contact: "6281234567890",
+  formFields: ["Player ID", "Nickname"],
+  items: [
+    { name: "16 Tokens", price: "Rp 4.000" },
+    { name: "80 Tokens", price: "Rp 16.300" }
+  ]
+};
