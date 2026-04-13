442ab50fa403ea4e264b0cec8dac68bbdb6c7699
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-14 04:03:21 +0700

    Create genshinmy.js

---
+++ data/genshinmy.js
@@ -0,0 +1,32 @@
+window.PRICE_DATA = {
+  title: "Genshin Impact (MY)",
+  subtitle: "Proses otomatis 1-10 menit, via UID & Server. Khusus region Malaysia.",
+  contact: "6283197962700",
+  formFields: [
+    { type: "text", label: "UID", placeholder: "Masukkan UID" },
+    {
+      type: "select",
+      label: "Server",
+      options: ["Asia", "America", "Europe", "TW/HK/MO"]
+    }
+  ],
+  categories: [
+    {
+      title: "Monthly Membership",
+      items: [
+        { name: "Welkin Moon", price: "Rp 73.213" }
+      ]
+    },
+    {
+      title: "Genesis Crystal",
+      items: [
+        { name: "60 GC", price: "Rp 15.091" },
+        { name: "330 (300+30) GC", price: "Rp 73.140" },
+        { name: "1090 (980+110) GC", price: "Rp 216.753" },
+        { name: "2240 (1980+260) GC", price: "Rp 461.440" },
+        { name: "3880 (3280+600) GC", price: "Rp 702.990" },
+        { name: "8080 (6480+1600) GC", price: "Rp 1.423.450" }
+      ]
+    },
+  ]
+};
