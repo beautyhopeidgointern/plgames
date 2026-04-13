cfddd4fcb79d0de61498d0c5a8536345db8ccc13
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-14 03:52:25 +0700

    Update genshinid.js

--- data/genshinid.js
+++ data/genshinid.js
@@ -1,5 +1,5 @@
 window.PRICE_DATA = {
-  title: "Genshin Impact (Indonesia)",
+  title: "Genshin Impact",
   subtitle: "Proses otomatis 1-10 menit, via UID & Server. Khusus region Indonesia.",
   contact: "6283197962700",
   formFields: [
@@ -12,10 +12,35 @@ window.PRICE_DATA = {
   ],
   categories: [
     {
-      title: "Topup",
+      title: "Monthly Membership",
       items: [
-        { name: "60 Genesis Crystals", price: "Rp 16.000" },
-        { name: "300 + 30 Genesis Crystals", price: "Rp 79.000" }
+        { name: "Welkin Moon", price: "Rp 66.500" },
+        { name: "Welkin Moon (×2)", price: "Rp 133.000" },
+        { name: "Welkin Moon (×3)", price: "Rp 199.500" },
+        { name: "Welkin Moon (×4)", price: "Rp 266.000" },
+        { name: "Welkin Moon (×5)", price: "Rp 332.500" }
+      ]
+    },
+    {
+      title: "Genesis Crystal",
+      items: [
+        { name: "60 GC", price: "Rp 15.000" },
+        { name: "330 (300+30) GC", price: "Rp 66.500" },
+        { name: "1090 (980+110) GC", price: "Rp 205.000" },
+        { name: "2240 (1980+260) GC", price: "Rp 399.000" },
+        { name: "3880 (3280+600) GC", price: "Rp 652.500" },
+        { name: "8080 (6480+1600) GC", price: "Rp 1.330.000" }
+      ]
+    },
+    {
+      title: "Chronal Nexus",
+      items: [
+        { name: "60 Chronal Nexus", price: "Rp 15.000" },
+        { name: "330 (300+30) Chronal Nexus", price: "Rp 66.500" },
+        { name: "1090 (980+110) Chronal Nexus", price: "Rp 205.000" },
+        { name: "2240 (1980+260) Chronal Nexus", price: "Rp 399.000" },
+        { name: "3880 (3280+600) Chronal Nexus", price: "Rp 652.500" },
+        { name: "8080 (6480+1600) Chronal Nexus", price: "Rp 1.330.000" }
       ]
     }
   ]
