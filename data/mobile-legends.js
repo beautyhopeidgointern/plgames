abbe523e0f82557f6c46615e6fd4007bd466ad9b
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-13 18:44:25 +0700

    Create mobile-legends.js

---
+++ data/mobile-legends.js
@@ -0,0 +1,14 @@
+window.PRICE_DATA = {
+  title: "Mobile Legends",
+  subtitle: "Top Up Diamond Murah dan Aman",
+  contact: "6281234567890",
+  formFields: ["User ID", "Zone ID", "Nickname"],
+  items: [
+    { name: "86 Diamond", price: "Rp 20.000" },
+    { name: "172 Diamond", price: "Rp 39.000" },
+    { name: "257 Diamond", price: "Rp 58.000" },
+    { name: "344 Diamond", price: "Rp 76.000" },
+    { name: "429 Diamond", price: "Rp 95.000" },
+    { name: "514 Diamond", price: "Rp 113.000" }
+  ]
+};
