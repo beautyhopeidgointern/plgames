35d76c2a5d11ec2de24c25bb7ec57813caf08087
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-13 18:51:07 +0700

    Create games.js

---
+++ data/games.js
@@ -0,0 +1,26 @@
+window.GAME_CATALOG = [
+  {
+    slug: "mlid",
+    name: "Mobile Legends",
+    icon: "ML",
+    description: "Top up diamond Mobile Legends dengan pilihan nominal lengkap."
+  },
+  {
+    slug: "hok",
+    name: "Honor of Kings",
+    icon: "HK",
+    description: "Price list Honor of Kings dengan format pesanan yang rapi."
+  },
+  {
+    slug: "ff",
+    name: "Free Fire",
+    icon: "FF",
+    description: "Top up diamond Free Fire cepat, simpel, dan mudah dipilih."
+  },
+  {
+    slug: "pubgm",
+    name: "PUBG Mobile",
+    icon: "PM",
+    description: "Price list UC PUBG Mobile dengan tampilan yang rapi."
+  }
+];
