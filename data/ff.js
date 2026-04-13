2ae25050d727b68595fc67e1e78ef239f55c99a7
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-13 20:14:47 +0700

    Update ff.js

--- data/ff.js
+++ data/ff.js
@@ -1,7 +1,7 @@
 window.PRICE_DATA = {
   title: "Free Fire",
   subtitle: "Top Up Diamond Free Fire",
-  contact: "6281234567890",
+  contact: "6293197962700",
   formFields: ["User ID", "Nickname"],
   items: [
     { name: "70 Diamond", price: "Rp 10.000" },
