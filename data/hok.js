f65824e92b86bb18e56899ac87a1c1725887fd5b
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-14 04:06:18 +0700

    Update hok.js

--- data/hok.js
+++ data/hok.js
@@ -1,10 +1,23 @@
 window.PRICE_DATA = {
   title: "Honor of Kings",
-  subtitle: "Top Up Token Murah",
+  subtitle: "Proses otomatis 1-10 menit, via Player ID",
   contact: "6283197962700",
-  formFields: ["Player ID", "Nickname"],
-  items: [
-    { name: "16 Tokens", price: "Rp 4.000" },
-    { name: "80 Tokens", price: "Rp 16.300" }
+  formFields: ["Player ID"],
+  categories: [
+    {
+      title: "Tokens",
+      items: [
+        { name: "16 Tokens", price: "Rp 3.500" },
+        { name: "80 Tokens", price: "Rp 15.950" },
+        { name: "240 Tokens", price: "Rp 46.573" },
+        { name: "400 Tokens", price: "Rp 77.642" },
+        { name: "560 Tokens", price: "Rp 109.412" },
+        { name: "830 Tokens", price: "Rp 156.594" },
+        { name: "1245 Tokens", price: "Rp 233.485" },
+        { name: "2508 Tokens", price: "Rp 463.397" },
+        { name: "4180 Tokens", price: "Rp 772.426" },
+        { name: "8360 Tokens", price: "Rp 1.555.796" }
+      ]
+    }
   ]
 };
