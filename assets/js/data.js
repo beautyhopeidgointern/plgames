cf95519f5d20aa40f15ab12416ba6b8c75d27d2d
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-13 14:55:15 +0700

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,35 @@
+const PRICE_LIST = {
+  "mobile-legends": {
+    title: "Mobile Legends",
+    subtitle: "Top Up Diamond Murah dan Aman",
+    contact: "6281234567890",
+    formFields: ["User ID", "Zone ID", "Nickname"],
+    items: [
+      { name: "86 Diamond", price: "Rp 20.000" },
+      { name: "172 Diamond", price: "Rp 39.000" },
+      { name: "257 Diamond", price: "Rp 58.000" }
+    ]
+  },
+
+  "hok": {
+    title: "Honor of Kings",
+    subtitle: "Top Up Tokens Murah",
+    contact: "6281234567890",
+    formFields: ["Player ID", "Nickname"],
+    items: [
+      { name: "16 Tokens", price: "Rp 4.000" },
+      { name: "80 Tokens", price: "Rp 16.300" }
+    ]
+  },
+
+  "free-fire": {
+    title: "Free Fire",
+    subtitle: "Top Up Diamond Free Fire",
+    contact: "6281234567890",
+    formFields: ["User ID", "Nickname"],
+    items: [
+      { name: "70 Diamond", price: "Rp 10.000" },
+      { name: "140 Diamond", price: "Rp 19.000" }
+    ]
+  }
+};
