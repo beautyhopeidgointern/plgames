b41d0026718f1c63238f14845f15458ee8b09e55
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-05-11 01:13:07 +0700

    Update hok.js

--- data/hok.js
+++ data/hok.js
@@ -5,18 +5,33 @@ window.PRICE_DATA = {
   formFields: ["Player ID"],
   categories: [
     {
+      title: "Membership & Packs",
+      items: [
+        { name: "Weekly Card", price: "Rp 17.297" },
+        { name: "Weekly Card Plus", price: "Rp 52.510" },
+        { name: "Double Token Lucky Bag", price: "Rp 5.073" },
+        { name: "Honor Point Value Pack", price: "Rp 5.073" },
+        { name: "Premium Purchase Rebate Pack", price: "Rp 21.119" },
+        { name: "Standard Purchase Rebate Pack", price: "Rp 5.073" }
+      ]
+    },
+    {
       title: "Tokens",
       items: [
-        { name: "16 Tokens", price: "Rp 3.500" },
-        { name: "80 Tokens", price: "Rp 15.950" },
-        { name: "240 Tokens", price: "Rp 46.573" },
-        { name: "400 Tokens", price: "Rp 77.642" },
-        { name: "560 Tokens", price: "Rp 109.412" },
-        { name: "830 Tokens", price: "Rp 156.594" },
-        { name: "1245 Tokens", price: "Rp 233.485" },
-        { name: "2508 Tokens", price: "Rp 463.397" },
-        { name: "4180 Tokens", price: "Rp 772.426" },
-        { name: "8360 Tokens", price: "Rp 1.555.796" }
+        { name: "80 Tokens", price: "Rp 15.388" },
+        { name: "240 Tokens", price: "Rp 45.396" },
+        { name: "400 Tokens", price: "Rp 75.035" },
+        { name: "560 Tokens", price: "Rp 105.642" },
+        { name: "800 + 30 Tokens", price: "Rp 150.558" },
+        { name: "830 (800 Token +30 Bonus)", price: "Rp 150.833" },
+        { name: "1.200 Tokens", price: "Rp 225.775" },
+        { name: "1200 + 45 Tokens", price: "Rp 224.082" },
+        { name: "2.400 Tokens", price: "Rp 443.787" },
+        { name: "2400 + 108 Tokens", price: "Rp 441.164" },
+        { name: "4.000 Tokens", price: "Rp 739.642" },
+        { name: "4000 + 180 Tokens", price: "Rp 735.366" },
+        { name: "8000 + 360 Tokens", price: "Rp 1.481.152" },
+        { name: "8360 (8000 Token +360 Bonus)", price: "Rp 1.479.255" }
       ]
     }
   ]
