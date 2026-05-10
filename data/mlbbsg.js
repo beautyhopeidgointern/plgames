e52a716510a58baed5d892178f6cd40494eeb16d
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-05-11 01:18:58 +0700

    Update mlbbsg.js

--- data/mlbbsg.js
+++ data/mlbbsg.js
@@ -7,27 +7,26 @@ window.PRICE_DATA = {
     {
       title: "Passes",
       items: [
-        { name: "Weekly Elite Bundle (1x seminggu)", price: "Rp 16.869" },
-        { name: "Weekly Diamond Pass (Event Topup +100)", price: "Rp 37.189" },
-        { name: "Monthly Epic Bundle (1x sebulan)", price: "Rp 84.745" }
+        { name: "Weekly Elite Bundle (1x seminggu)", price: "Rp 16.541" },
+        { name: "Weekly Diamond Pass (Event Topup +100)", price: "Rp 36.396" },
+        { name: "Monthly Epic Bundle (1x sebulan)", price: "Rp 81.855" }
       ]
     },
     {
       title: "Diamonds",
       items: [
-        { name: "14 Diamonds (13 + 1 Bonus)", price: "Rp 4.297" },
-        { name: "28 Diamonds (26 + 2 Bonus)", price: "Rp 8.587" },
-        { name: "112 Diamond (102 + 10 Bonus)", price: "Rp 34.300" },
-        { name: "140 Diamonds (127 + 13 Bonus)", price: "Rp 42.769" },
-        { name: "284 Diamonds (254 + 30 Bonus)", price: "Rp 85.810" },
-        { name: "355 Diamonds (317 + 38 Bonus)", price: "Rp 106.000" },
-        { name: "429 Diamonds (383 + 46 Bonus)", price: "Rp 127.000" },
-
-        { name: "716 Diamonds (633 + 83 Bonus)", price: "Rp 211.000" },
-        { name: "1084 Diamonds (940 + 144 Bonus)", price: "Rp 322.000" },
-        { name: "1446 Diamonds (1252 + 194 Bonus)", price: "Rp 426.000" },
-
-        { name: "2976 Diamonds (2501 + 475 Bonus)", price: "Rp 847.000" }
+        { name: "14 Diamonds (13 + 1 Bonus)", price: "Rp 4.653" },
+        { name: "28 Diamonds (26 + 2 Bonus)", price: "Rp 8.825" },
+        { name: "112 Diamond (102 + 10 Bonus)", price: "Rp 34.017" },
+        { name: "140 Diamonds (127 + 13 Bonus)", price: "Rp 42.386" },
+        { name: "284 Diamonds (254 + 30 Bonus)", price: "Rp 84.907" },
+        { name: "355 Diamonds (317 + 38 Bonus)", price: "Rp 105.454" },
+        { name: "429 Diamonds (383 + 46 Bonus)", price: "Rp 126.805" },
+        { name: "716 Diamonds (633 + 83 Bonus)", price: "Rp 210.170" },
+        { name: "1084 Diamonds (940 + 144 Bonus)", price: "Rp 316.623" },
+        { name: "1446 Diamonds (1252 + 194 Bonus)", price: "Rp 410.060" },
+        { name: "2976 Diamonds (2501 + 475 Bonus)", price: "Rp 803.663" },
+        { name: "7502 Diamonds (6252 + 1250 Bonus)", price: "Rp 1.991.805" }
       ]
     }
   ]
