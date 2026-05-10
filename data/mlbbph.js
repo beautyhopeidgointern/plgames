51b5d6b9e92aa0d68e2a59080f2529e15c9eb403
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-05-11 01:16:30 +0700

    Update mlbbph.js

--- data/mlbbph.js
+++ data/mlbbph.js
@@ -7,54 +7,62 @@ window.PRICE_DATA = {
     {
       title: "First Topup",
       items: [
-        { name: "50 Diamonds + 50 Diamonds (1x Purchase)", price: "Rp 14.489" },
-        { name: "150 Diamonds + 150 Diamonds (1x Purchase)", price: "Rp 43.635" },
-        { name: "250 Diamonds + 250 Diamonds (1x Purchase)", price: "Rp 72.511" },
-        { name: "500 Diamonds + 500 Diamonds (1x Purchase)", price: "Rp 145.228" }
+        { name: "50 Diamonds + 50 Diamonds (1x Purchase)", price: "Rp 13.901" },
+        { name: "150 Diamonds + 150 Diamonds (1x Purchase)", price: "Rp 42.124" },
+        { name: "250 Diamonds + 250 Diamonds (1x Purchase)", price: "Rp 69.999" },
+        { name: "500 Diamonds + 500 Diamonds (1x Purchase)", price: "Rp 140.986" }
       ]
     },
     {
       title: "Passes",
       items: [
-        { name: "Weekly Elite Bundle (1x seminggu)", price: "Rp 14.470" },
-        { name: "Weekly Diamond Pass", price: "Rp 28.703" },
-        { name: "Weekly Diamond Pass (Event Topup +100) x2", price: "Rp 57.432" },
-        { name: "Monthly Epic Bundle", price: "Rp 70.287" },
-        { name: "Weekly Diamond Pass (Event Topup +100) x3", price: "Rp 86.160" },
-        { name: "Weekly Diamond Pass (Event Topup +100) x4", price: "Rp 114.882" },
-        { name: "Weekly Diamond Pass (Event Topup +100) x5", price: "Rp 139.133" },
-        { name: "Twilight Pass", price: "Rp 143.094" }
+        { name: "Weekly Elite Bundle (1x seminggu)", price: "Rp 13.981" },
+        { name: "Weekly Diamond Pass", price: "Rp 27.420" },
+        { name: "Weekly Diamond Pass (Event Topup +100) x2", price: "Rp 54.366" },
+        { name: "Weekly Diamond Pass (Event Topup +100) x3", price: "Rp 81.548" },
+        { name: "Weekly Diamond Pass (Event Topup +100) x4", price: "Rp 108.201" },
+        { name: "Weekly Diamond Pass (Event Topup +100) x5", price: "Rp 133.971" },
+        { name: "Monthly Epic Bundle", price: "Rp 67.371" },
+        { name: "Twilight Pass", price: "Rp 136.623" }
       ]
     },
     {
       title: "Diamonds",
       items: [
-        { name: "5 Diamonds", price: "Rp 1.421" },
-        { name: "11 Diamonds", price: "Rp 2.928" },
-        { name: "22 Diamonds", price: "Rp 5.857" },
-        { name: "56 Diamonds", price: "Rp 14.598" },
-        { name: "112 Diamonds", price: "Rp 29.190" },
-        { name: "168 Diamonds (153 + 15 Bonus)", price: "Rp 42.151" },
-        { name: "223 Diamonds", price: "Rp 56.190" },
-        { name: "279 Diamonds (254 + 25 Bonus)", price: "Rp 70.335" },
-        { name: "301 Diamonds (274 + 27 Bonus)", price: "Rp 75.595" },
-        { name: "336 Diamonds", price: "Rp 87.115" },
-        { name: "448 Diamonds (405 + 43 Bonus)", price: "Rp 112.441" },
-
-        { name: "570 Diamonds", price: "Rp 144.323" },
-        { name: "626 Diamonds (555 + 71 Bonus)", price: "Rp 153.034" },
-        { name: "682 Diamonds (606 + 76 Bonus)", price: "Rp 166.946" },
-        { name: "738 Diamonds (657 + 81 Bonus)", price: "Rp 180.858" },
-        { name: "749 Diamonds (667 + 82 Bonus)", price: "Rp 183.613" },
-        { name: "793 Diamonds (707 + 86 Bonus)", price: "Rp 194.739" },
-        { name: "849 Diamonds (758 + 91 Bonus)", price: "Rp 208.650" },
-        { name: "906 Diamonds (807 + 99 Bonus)", price: "Rp 222.562" },
-        { name: "1018 Diamonds (909 + 109 Bonus)", price: "Rp 250.516" },
-        { name: "1163 Diamonds", price: "Rp 278.354" },
-
-        { name: "1733 Diamonds (1511 + 222 Bonus)", price: "Rp 417.526" },
-        { name: "2398 Diamonds", price: "Rp 556.482" },
-        { name: "3561 Diamonds (3022 + 539 Bonus)", price: "Rp 834.697" }
+        { name: "5 Diamonds", price: "Rp 1.833" },
+        { name: "11 Diamonds", price: "Rp 3.239" },
+        { name: "22 Diamonds", price: "Rp 5.977" },
+        { name: "56 Diamonds", price: "Rp 14.296" },
+        { name: "112 Diamonds", price: "Rp 28.101" },
+        { name: "168 Diamonds (153 + 15 Bonus)", price: "Rp 40.798" },
+        { name: "223 Diamonds", price: "Rp 54.396" },
+        { name: "279 Diamonds (254 + 25 Bonus)", price: "Rp 67.994" },
+        { name: "301 Diamonds (274 + 27 Bonus)", price: "Rp 73.434" },
+        { name: "336 Diamonds", price: "Rp 83.640" },
+        { name: "448 Diamonds (405 + 43 Bonus)", price: "Rp 108.259" },
+        { name: "570 Diamonds", price: "Rp 138.676" },
+        { name: "626 Diamonds (555 + 71 Bonus)", price: "Rp 148.854" },
+        { name: "682 Diamonds (606 + 76 Bonus)", price: "Rp 162.388" },
+        { name: "738 Diamonds (657 + 81 Bonus)", price: "Rp 175.919" },
+        { name: "749 Diamonds (667 + 82 Bonus)", price: "Rp 178.625" },
+        { name: "793 Diamonds (707 + 86 Bonus)", price: "Rp 189.451" },
+        { name: "849 Diamonds (758 + 91 Bonus)", price: "Rp 202.984" },
+        { name: "906 Diamonds (807 + 99 Bonus)", price: "Rp 216.270" },
+        { name: "1018 Diamonds (909 + 109 Bonus)", price: "Rp 242.804" },
+        { name: "1163 Diamonds", price: "Rp 269.339" },
+        { name: "1733 Diamonds (1511 + 222 Bonus)", price: "Rp 402.006" },
+        { name: "2398 Diamonds", price: "Rp 535.983" },
+        { name: "3561 Diamonds (3022 + 539 Bonus)", price: "Rp 803.974" },
+        { name: "4131 Diamonds (3526 + 605 Bonus)", price: "Rp 937.967" },
+        { name: "5132 Diamonds (4333 + 799 Bonus)", price: "Rp 1.152.361" },
+        { name: "6042 Diamonds", price: "Rp 1.339.953" },
+        { name: "7205 Diamonds (6042 + 1163 Bonus)", price: "Rp 1.607.944" },
+        { name: "8440 Diamonds (7050 + 1390 Bonus)", price: "Rp 1.875.934" },
+        { name: "9010 Diamonds (7554 + 1456 Bonus)", price: "Rp 2.009.930" },
+        { name: "10173 Diamonds (8561 + 1612 Bonus)", price: "Rp 2.277.920" },
+        { name: "12084 Diamonds (10070 + 2014 Bonus)", price: "Rp 2.679.904" },
+        { name: "15052 Diamonds (12589 + 2463 Bonus)", price: "Rp 3.349.881" },
+        { name: "20524 Diamonds (17120 + 3404 Bonus)", price: "Rp 4.555.838" }
       ]
     }
   ]
