7c19be14862363190a7b18adf1779308ec695f33
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-05-11 01:08:25 +0700

    Update ffmysg.js

--- data/ffmysg.js
+++ data/ffmysg.js
@@ -7,28 +7,19 @@ window.PRICE_DATA = {
     {
       title: "Membership",
       items: [
-        { name: "Weekly Lite", price: "Rp 6.773" },
-        { name: "Weekly Membership", price: "Rp 29.005" },
-        { name: "Monthly Membership", price: "Rp 137.770" }
+        { name: "Weekly Lite", price: "Rp 5.982" },
+        { name: "Weekly", price: "Rp 26.785" },
+        { name: "Monthly", price: "Rp 131.698" },
+        { name: "Evo Acces 3D", price: "Rp 9.535" },
+        { name: "Evo Acces 7D", price: "Rp 14.252" },
+        { name: "Evo Acces 30D", price: "Rp 41.618" }
       ]
     },
     {
       title: "Topup Diamonds",
       items: [
-        { name: "25 Diamonds", price: "Rp 5.234" },
-        { name: "50 Diamonds", price: "Rp 9.409" },
-        { name: "100 Diamonds", price: "Rp 17.286" },
-        { name: "200 Diamonds", price: "Rp 33.106" },
-        { name: "300 Diamonds", price: "Rp 48.859" },
-        { name: "520 Diamonds", price: "Rp 81.570" },
-        { name: "830 Diamonds", price: "Rp 130.012" },
-        { name: "930 Diamonds", price: "Rp 146.138" },
-        { name: "1060 Diamonds", price: "Rp 160.245" },
-        { name: "1580 Diamonds", price: "Rp 240.876" },
-        { name: "2180 Diamonds", price: "Rp 322.407" },
-        { name: "3240 Diamonds", price: "Rp 482.430" },
-        { name: "5600 Diamonds", price: "Rp 794.370" },
-        { name: "11500 Diamonds", price: "Rp 1.633.380" }
+        { name: "25 Diamonds", price: "Rp 4.460" },
+        { name: "50 Diamonds", price: "Rp 8.418" }
       ]
     }
   ]
