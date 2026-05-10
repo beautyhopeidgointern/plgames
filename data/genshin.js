abf3b8c3d63988be365c7964d35b7e6186ba7c52
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-05-11 01:10:00 +0700

    Update genshin.js

--- data/genshin.js
+++ data/genshin.js
@@ -14,8 +14,13 @@ window.PRICE_DATA = {
     {
       title: "Topup",
       items: [
-        { name: "60 Genesis Crystals", price: "Rp 16.000" },
-        { name: "300 + 30 Genesis Crystals", price: "Rp 79.000" }
+        { name: "Blessing Welkin Moon", price: "Rp 71.138" },
+        { name: "60 Crystals", price: "Rp 15.418" },
+        { name: "300+30 Crystals", price: "Rp 71.138" },
+        { name: "980+110 Crystals", price: "Rp 221.182" },
+        { name: "1980+260 Crystals", price: "Rp 419.491" },
+        { name: "3280+600 Crystals", price: "Rp 700.106" },
+        { name: "6480+1600 Crystals", price: "Rp 1.403.337" }
       ]
     }
   ]
