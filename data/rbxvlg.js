2cd82f1d0f39cf56dcefb7a291d38ca86638825e
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-06-22 09:24:14 +0700

    Update rbxvlg.js

--- data/rbxvlg.js
+++ data/rbxvlg.js
@@ -9,43 +9,44 @@ window.PRICE_DATA = {
       items: [
         { name: "80 Robux", price: "Rp 16.000" },
         { name: "160 Robux", price: "Rp 32.000" },
-        { name: "240 Robux", price: "Rp 46.500" },
-        { name: "320 Robux", price: "Rp 60.500" }
+        { name: "240 Robux", price: "Rp 47.500" },
+        { name: "320 Robux", price: "Rp 62.000" }
       ]
     },
     {
       title: "Basic",
       items: [
-        { name: "500 Robux", price: "Rp 74.000" },
-        { name: "580 Robux", price: "Rp 90.000" },
-        { name: "660 Robux", price: "Rp 106.000" },
-        { name: "740 Robux", price: "Rp 120.500" },
-        { name: "820 Robux", price: "Rp 134.500" },
-        { name: "1000 Robux", price: "Rp 148.000" },
-        { name: "1240 Robux", price: "Rp 194.500" },
-        { name: "1500 Robux", price: "Rp 222.000" },
-        { name: "2000 Robux", price: "Rp 296.000" },
-        { name: "2500 Robux", price: "Rp 370.000" },
-        { name: "3000 Robux", price: "Rp 444.000" },
-        { name: "3500 Robux", price: "Rp 518.000" },
-        { name: "4000 Robux", price: "Rp 592.000" },
-        { name: "4500 Robux", price: "Rp 666.000" },
-        { name: "5000 Robux", price: "Rp 740.000" },
-        { name: "5500 Robux", price: "Rp 814.000" },
-        { name: "6000 Robux", price: "Rp 888.000" },
-        { name: "6500 Robux", price: "Rp 962.000" },
-        { name: "7000 Robux", price: "Rp 1.036.000" },
-        { name: "7500 Robux", price: "Rp 1.110.000" },
-        { name: "10000 Robux", price: "Rp 1.480.000" },
-        { name: "13000 Robux", price: "Rp 1.924.000" },
-        { name: "22500 Robux", price: "Rp 3.330.000" }
+        { name: "500 Robux", price: "Rp 75.500" },
+        { name: "580 Robux", price: "Rp 91.500" },
+        { name: "660 Robux", price: "Rp 107.500" },
+        { name: "740 Robux", price: "Rp 123.000" },
+        { name: "820 Robux", price: "Rp 137.500" },
+        { name: "1000 Robux", price: "Rp 151.000" },
+        { name: "1240 Robux", price: "Rp 198.500" },
+        { name: "1500 Robux", price: "Rp 226.500" },
+        { name: "2000 Robux", price: "Rp 302.000" },
+        { name: "2500 Robux", price: "Rp 377.500" },
+        { name: "3000 Robux", price: "Rp 453.000" },
+        { name: "3500 Robux", price: "Rp 528.500" },
+        { name: "4000 Robux", price: "Rp 604.000" },
+        { name: "4500 Robux", price: "Rp 679.500" },
+        { name: "5000 Robux", price: "Rp 755.000" },
+        { name: "5500 Robux", price: "Rp 830.500" },
+        { name: "6000 Robux", price: "Rp 906.000" },
+        { name: "6500 Robux", price: "Rp 981.500" },
+        { name: "7000 Robux", price: "Rp 1.057.000" },
+        { name: "7500 Robux", price: "Rp 1.132.500" },
+        { name: "10000 Robux", price: "Rp 1.510.000" },
+        { name: "13000 Robux", price: "Rp 1.963.000" },
+        { name: "15000 Robux", price: "Rp 2.265.000" },
+        { name: "22500 Robux", price: "Rp 3.397.500" }
       ]
     },
     {
       title: "Roblox Plus Region Indo (1 Bulan)",
       items: [
         { name: "Roblox Plus", price: "Rp 95.000" },
-        { name: "500r + Roblox Plus", price: "Rp 169.000" }
+        { name: "500r + Roblox Plus", price: "Rp 170.500" }
       ]
     }
   ]
